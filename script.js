2b096554d16a2ba22e6bb6354ccaded5e44934d6
Aziz-84 <azca0001@gmail.com>
2022-11-13 16:40:40 +0100

    fixed logic

--- script.js
+++ script.js
@@ -1,52 +1,57 @@
 let playerSelection = "";
 let computerSelection = "";
-
+let playerScore = 0;
+let computerScore = 0;
+let playing = false;
 const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
 const playerSelected = document.querySelector(".playerSelected");
 const computerSelected = document.querySelector(".computerSelected");
 console.log(rock);
+
 rock.addEventListener("click", (e) => {
+  playing = true;
   console.log(e.target);
-  playerSelection = "rock";
+  playerSelection = "rock ✊";
   playRound(playerSelection);
 });
 
 paper.addEventListener("click", (e) => {
   console.log(e.target);
-  playerSelection = "paper";
+  playerSelection = "paper ✋";
   playRound(playerSelection);
 });
 scissors.addEventListener("click", (e) => {
   console.log(e.target);
-  playerSelection = "scissors";
+  playerSelection = "scissors ✌️";
   playRound(playerSelection);
 });
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
   console.log(playerSelection);
-  let playerScore = 0;
-  let computerScore = 0;
 
-  // for (let i = 0; i < 5; i++) {
+  computerSelection = getComputerChoice();
   console.log(playerScore);
   console.log(computerScore);
-  computerSelection = getComputerChoice();
-
-  //playerSelection = prompt("Please choose: rock,paper or scissors ");
 
   playerSelected.textContent = "You selected" + "  " + playerSelection;
 
   computerSelected.textContent = "computer Selected" + "  " + computerSelection;
 
-  if (computerSelection == "rock" && playerSelection == "scissors") {
+  if (computerSelection == "rock ✊" && playerSelection == "scissors ✌️") {
     computerScore++;
     console.log("You Lose! rock beats scissors");
-  } else if (computerSelection === "scissors" && playerSelection === "paper") {
+  } else if (
+    computerSelection === "scissors ✌️" &&
+    playerSelection === "paper ✋"
+  ) {
     computerScore++;
     console.log("You Lose! Scissors beats Paper");
-  } else if (computerSelection === "paper" && playerSelection === "rock") {
+  } else if (
+    computerSelection === "paper ✋" &&
+    playerSelection === "rock ✊"
+  ) {
     computerScore++;
     console.log("You Lose! Paper beats Rock");
   } else if (computerSelection === playerSelection) {
@@ -56,31 +61,31 @@ function playRound(playerSelection, computerSelection) {
     console.log("You win!");
   }
 }
-if (playerScore > computerScore) {
-  console.log("Game over");
+if (playerScore === 3) {
   console.log("Player wins the game");
-} else {
+  playing = false;
+} else if (computerScore === 3) {
+  console.log("Game over");
   console.log("Computer wins the game");
+  playing = false;
 }
 
 console.log("computer scored:", computerScore);
 console.log("You scored:", playerScore);
-//}
 
 function getComputerChoice() {
   let random = Math.floor(Math.random() * 3 + 1);
 
   switch (random) {
     case 1:
-      return "rock";
+      return "rock ✊";
 
     case 2:
-      return "paper";
+      return "paper ✋";
 
     case 3:
-      return "scissors";
+      return "scissors ✌️";
     default:
       break;
   }
 }
-//playRound(playerSelection, computerSelection);
